0cda372948fc6bc0ab30403932dd26778df3b74d
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-07-25 23:23:00 +0300

    Add files via upload

---
+++ MOD-9_module_9_3.py
@@ -0,0 +1,12 @@
+
+# Домашнее задание по теме "Генераторные сборки
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(a) - len(b) for a, b in zip(first, second) if len(a) != len(b))
+second_result = (first[i] == second[i] for i in range(len(first)))
+
+print(list(first_result))
+print(list(second_result))
+
